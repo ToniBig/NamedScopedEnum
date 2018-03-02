1cd54cc3740f24594218fe54f47261ecac60bf8e
Tino Bog <tino.bog@gmail.com>
2018-03-02 20:52:10 +0100

    Even more formatting

--- example/static_map.cpp
+++ example/static_map.cpp
@@ -19,7 +19,7 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#include "named_scoped_enum.hpp"
+#include "../named_scoped_enum.hpp"
 #include <array>
 #include <iostream>
 
